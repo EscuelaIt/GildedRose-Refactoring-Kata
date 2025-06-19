05dda62804fabc1b354b69f64f469facaae91308
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-19 19:44:09 +0200

    refactor: introduce variable of max item quality

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -21,48 +21,15 @@ public class GildedRose
 
     void UpdateQualityOfItemByIndex(int i)
     {
-        if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-        {
-            if (Items[i].Quality > 0)
-            {
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].Quality = Items[i].Quality - 1;
-                }
-            }
-        }
-        else
-        {
-            if (Items[i].Quality < 50)
-            {
-                Items[i].Quality = Items[i].Quality + 1;
+        aklsjdflkadjf(i);
+        DecreaseSellInByOneIfNotSulfuras(i);
+        kdjfkajsd(i);
+    }
 
-                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].SellIn < 11)
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-
-                    if (Items[i].SellIn < 6)
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-        {
-            Items[i].SellIn = Items[i].SellIn - 1;
-        }
-
+    void kdjfkajsd(int i)
+    {
+        var maxItemQuality = 50;
+        
         if (Items[i].SellIn < 0)
         {
             if (Items[i].Name != "Aged Brie")
@@ -84,10 +51,70 @@ public class GildedRose
             }
             else
             {
-                if (Items[i].Quality < 50)
+                if (Items[i].Quality < maxItemQuality)
                 {
                     Items[i].Quality = Items[i].Quality + 1;
                 }
+            }
+        }
+    }
+
+    void DecreaseSellInByOneIfNotSulfuras(int i)
+    {
+        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+        {
+            Items[i].SellIn = Items[i].SellIn - 1;
+        }
+    }
+
+    void aklsjdflkadjf(int i)
+    {
+        if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+        {
+            DecrementQualityByOneIfNotSulfuras(i);
+        }
+        else
+        {
+            jkjfaklsd(i);
+        }
+    }
+
+    void jkjfaklsd(int i)
+    {
+        var maxItemQuality = 50;
+        
+        if (Items[i].Quality < maxItemQuality)
+        {
+            Items[i].Quality = Items[i].Quality + 1;
+
+            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (Items[i].SellIn < 11)
+                {
+                    if (Items[i].Quality < maxItemQuality)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+                    }
+                }
+
+                if (Items[i].SellIn < 6)
+                {
+                    if (Items[i].Quality < maxItemQuality)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    void DecrementQualityByOneIfNotSulfuras(int i)
+    {
+        if (Items[i].Quality > 0)
+        {
+            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            {
+                Items[i].Quality -= 1;
             }
         }
     }
