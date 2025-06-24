e1e5ed48de2a83f0c059b0c52b8c33156e8fddb8
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-24 19:22:15 +0200

    refactor: use sulfuras factory method

--- csharp.NUnit/GildedRose/Sulfuras.cs
+++ csharp.NUnit/GildedRose/Sulfuras.cs
@@ -2,6 +2,18 @@
 
 public class Sulfuras : Item
 {
+    Sulfuras(int sellIn, int quality)
+    {
+        Name = "Sulfuras, Hand of Ragnaros";
+        SellIn = sellIn;
+        Quality = quality;
+    }
+
+    public static Sulfuras With(int sellIn, int quality)
+    {
+        return new Sulfuras(sellIn, quality);
+    }
+
     public override void DecrementQuality()
     {
         //Sulfuras is legendary and does not decrease in quality.
